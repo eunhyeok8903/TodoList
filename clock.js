c3339d0a3bb62df58012632cf435cb8df6fcaef7
eunhyeok <eunhyeok8903@gmail.com>
2021-01-14 16:14:18 +0900

    매초마다 시간 업데이트 구현

--- clock.js
+++ clock.js
@@ -1,16 +1,19 @@
+//html에서 태그와 class이름을 이용하여 변수 저장
 const clockContainer = document.querySelector(".js-clock"),
-clockTitle = clockContainer.querySelector("h1");
+clockTitle = clockContainer.querySelector("h1"); //clockContainer에서 h1 태그 변수 저장(시간 나타낼 부분)
 
+//Date함수 이용하여 clockTitle에 시간 text입력
 function getTime() {
-  const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours<10 ? `0${hours}` : `${hours}`}:${minutes<10 ? `0${minutes}` : `${minutes}`}:${seconds <10 ? `0${seconds}` : `${seconds}`}`;
 }
 
 function init() {
-  getTime();
+    //1000ms마다 getTime실행
+    setInterval(getTime,1000); 
 }
 
 init();
