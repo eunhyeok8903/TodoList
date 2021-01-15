e14577dec88cee2ec28f6d113b5b00568f3d00e2
eunhyeok <eunhyeok8903@gmail.com>
2021-01-15 16:29:41 +0900

    todo 추가 임시 구현

--- gretting.js
+++ gretting.js
@@ -11,7 +11,7 @@ function saveName(text){
     localStorage.setItem(USER_LS,text);
 }
 
-//submit 이벤트 함수
+//submit 클릭 이벤트 함수
 function handleSubmit(event){
     event.preventDefault(); //form에 Enter입력시 사라지는 기본동작 prevent
     const currentValue=input.value;
@@ -28,7 +28,7 @@ function askForName(){
 //greeting 함수
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN); //폼은 숨기고 (What is your name 폼 지워버림)
-    greeting.classList.add(SHOWING_CN); // greeting 인사말은 추가해야함
+    greeting.classList.add(SHOWING_CN); // js-greeting h4 하위에 class showing추가
     greeting.innerText=`Hello ${text}`;
 }
 
---
+++ todo.js
@@ -0,0 +1,38 @@
+toDoForm = document.querySelector(".js-toDoForm"),
+toDoInput= toDoForm.querySelector("input"),
+toDoList = document.querySelector(".js-toDoList");
+
+const TODO_LS="toDos";
+
+function paintTodo(text){
+    const li=document.createElement("li");
+    const delBtn=document.createElement("button");
+    const span = document.createElement("span");
+
+    delBtn.innerText="❌";
+    span.innerText=text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue=toDoInput.value;
+    paintTodo(currentValue);
+    toDoInput.value="";
+}
+
+function loadToDos(){
+    const toDos= localStorage.getItem(TODO_LS);
+    if(toDos!==null){
+
+    }
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit",handleSubmit);
+}
+
+init();
