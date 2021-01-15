bb997a948a1d8b7f9f103a820c775cd872960660
eunhyeok <eunhyeok8903@gmail.com>
2021-01-15 17:06:23 +0900

    todo리스트 localStorage 로드 적용

--- todo.js
+++ todo.js
@@ -4,18 +4,39 @@ toDoList = document.querySelector(".js-toDoList");
 
 const TODO_LS="toDos";
 
+const toDos=[];
+
+function saveToDos(){
+//localStorage에는 js 데이터를 저장할 수 없다.
+//String으로만 저장 가능
+localStorage.setItem(TODO_LS,JSON.stringify(toDos));
+}
+
+//text인자를 todo리스트에 추가하는 함수
 function paintTodo(text){
+    //html태그 생성
     const li=document.createElement("li");
     const delBtn=document.createElement("button");
     const span = document.createElement("span");
+    const newId = toDos.length+1;
 
+    //값 입력
     delBtn.innerText="❌";
     span.innerText=text;
     li.appendChild(span);
     li.appendChild(delBtn);
     toDoList.appendChild(li);
+    const toDoObj={
+        text: text,
+        id: newId
+    }
+    //toDos추가
+    toDos.push(toDoObj); 
+    saveToDos();
+
 }
 
+//submit 이벤트 함수
 function handleSubmit(event){
     event.preventDefault();
     const currentValue=toDoInput.value;
@@ -23,10 +44,15 @@ function handleSubmit(event){
     toDoInput.value="";
 }
 
+//local에 저장된 toDos 로드(paint)하는 함수 
 function loadToDos(){
-    const toDos= localStorage.getItem(TODO_LS);
-    if(toDos!==null){
-
+    const loadedToDos= localStorage.getItem(TODO_LS);
+    if(loadedToDos!==null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            //forEach로 모든 원소들 text를 todoList에 씀
+            paintTodo(toDo.text)
+        })
     }
 }
 
