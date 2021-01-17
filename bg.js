2a0d19baf0cd45061293dfa7a6b73b18f12ee54e
eunhyeok <eunhyeok8903@gmail.com>
2021-01-17 20:18:57 +0900

    랜덤 배경 추가

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER=6;
+
+//1~IMG_NUMBER 숫자 반환 함수
+function getRandom(){
+    const number = Math.floor(Math.random()*IMG_NUMBER);
+    return number+1;
+}
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src=`images/${imgNumber}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+    
+}
+
+function init(){
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+}
+
+init();
